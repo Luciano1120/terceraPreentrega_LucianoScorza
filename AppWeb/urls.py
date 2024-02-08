f84dd09f4c5f78afa8a1852ea42fe331968551ce
LucianoS <lucianoscorza@hotmail.com>
2024-02-08 01:03:05 -0300

    mejoramos funcionalidad

--- AppWeb/urls.py
+++ AppWeb/urls.py
@@ -37,8 +37,8 @@ urlpatterns = [
     path("editProv/<provNombre>", editarProv, name="EditarProv"), #la llamo desde el Boton en el html leerProv.html
 
     #CRUD Item con vistas basados en Clase
-    path("listaItem/", ListaItem.as_view()), # a diferencia de las views basadas en funciones de agrega el nombre de la clase.as_view()
-    path("agregaItem/", CreaItem.as_view()), 
+    path("listaItem/", ListaItem.as_view(), name="b_item"), # a diferencia de las views basadas en funciones de agrega el nombre de la clase.as_view()
+    path("agregaItem/", CreaItem.as_view(), name="a_item"), 
     path("actualizaItem/<int:pk>", ActualizaItem.as_view()),  #le tengo q poner el paramentro del id del Item en este caso el id 3 http://127.0.0.1:8000/AppWeb/actualizaItem/3
     path("borraItem/<int:pk>", BorrarItem.as_view()),
 
